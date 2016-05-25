88a10e737e28b8cc962dc41f66d771fea722c811
maniferrari <maniferrari@gmail.com>
2016-05-25 08:55:38 -0700

    first commit

---
+++ TipsCalc WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TipsCalc WatchKit Extension
+//
+//  Created by Manikandan Mohan on 3/10/15.
+//  Copyright (c) 2015 WristGlance. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
